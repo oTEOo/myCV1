166c00b77026f6356e9051c38e9222e8ba26cc6b
oTEOo <1851060158@e.tlu.edu.vn>
2021-01-13 22:25:21 +0700

    CV with html css

---
+++ myOnlineCV/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Online CV</title>
+</head>
+<body>
+    <div class="profile-img">
+    <img src="images/user.png" alt="profile-img">
+    </div>
+
+    <div class="profile-info">
+
+    <h1>Nguyễn Trung Hiếu</h1>
+    <p class="about">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem, molestiae error. Nam, voluptas dolore molestiae soluta officia vel architecto. Quos, perferendis sit? Qui, ad quas corporis saepe fugiat incidunt culpa.</p>
+    <div class="social-media">
+    <img src="images/facebook-icon.png" alt="facebook icon">
+    <img src="images/twitter-icon.png" alt="twitter icon">
+    <img src="images/whatsapp-icon.png" alt="whatsapp icon">
+    </div>
+    
+    <h2>Expertise</h2>
+    <div class="expertise">
+    <img src="images/web.png" alt="web">
+    <p><b>Web Designing</b></p>
+    <small>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet ipsa temporibus voluptate delectus doloribus, tempore rem maiores dignissimos, ipsum facilis nobis, soluta totam quo quisquam! Totam quasi iste recusandae a!
+    </small>
+    </div>
+    <div class="expertise">
+    <img src="images/marketing.png" alt="marketing">
+    <p><b>Digital Marketing</b></p>
+    <small>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet ipsa temporibus voluptate delectus doloribus, tempore rem maiores dignissimos, ipsum facilis nobis, soluta totam quo quisquam! Totam quasi iste recusandae a!
+    </small>
+    </div>
+    <div class="expertise">
+    <img src="images/graphics.png" alt="graphics">
+    <p><b>Graphics Designing</b></p>
+    <small>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet ipsa temporibus voluptate delectus doloribus, tempore rem maiores dignissimos, ipsum facilis nobis, soluta totam quo quisquam! Totam quasi iste recusandae a!
+    </small>
+    </div>
+
+    <h2>My Project Work</h2>
+    
+    <div class="projects">
+    <img src="images/1.png" alt="1">
+    <img src="images/2.png" alt="2">
+    <img src="images/3.png" alt="3">
+    <img src="images/4.png" alt="4">
+    <img src="images/5.png" alt="5">
+    <img src="images/6.png" alt="6">
+    </div>
+
+    <div class="form-box">
+
+    <h2>Contact me</h2>
+    <form action="">
+        <div class="input-group">
+        <input type="text" placeholder="first name">
+        <input type="text" placeholder="last name">
+        </div>
+        <div class="input-group">
+        <input type="number" placeholder="phone number">
+        <input type="email" placeholder="email">
+        </div>
+        <div class="text-area">
+        <textarea rows="4" placeholder="your message"></textarea>
+        </div>
+        <button type="submit" class="submit-btn">SEND MESSAGE</button>
+    </form>
+
+    </div>
+    
+    <div class="contact">
+        <span><img src="images/gmail.png" alt="gmail">hello@website.com</span>
+        <span><img src="images/phone.png" alt="phone">+12345677889</span>
+    </div>
+
+    <p class="copyright">Designed by Nguyễn Trung Hiếu</p>
+
+    </div>
+
+    
+
+</body>
+</html>
